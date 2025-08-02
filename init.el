9f177891e7128f8e06616e5f4d348c0693debb6d
WildSource <ilovetrap123@hotmail.com>
2025-08-01 23:42:13 -0400

    add ido mode

--- init.el
+++ init.el
@@ -6,6 +6,9 @@
         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
+;; autocompletion on file searching in dired mode
+(ido-mode 1)
+
 ;; Remove tool-bar
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -165,10 +168,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(autothemer exec-path-from-shell fireplace haskell-mode
-		magit nyan-mode org-modern pdf-tools php-mode
-		tx vterm web-mode))
+ '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
+ '(package-selected-packages nil)
  '(warning-suppress-log-types '((use-package))))
 
 (custom-set-faces
